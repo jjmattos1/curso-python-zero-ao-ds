b3db4f0e0b7b8e6df370ba09ca47996755a1ee4a
Jonas Mattos <brainstorm@jonasmattos.com.br>
2022-02-25 13:02:32 -0300

    Começo do script e notas da aula2.

--- exemplo02.py
+++ exemplo02.py
@@ -1,3 +1,47 @@
 # script aula02 do Curso Python do Zero ao DS do Meigarom (Seja um Datascientist).
 #
-# Q1
+# importar/declarar a library Pandas e carregar o DataFrame para memória
+#
+import pandas as pd
+
+data = pd.read_csv('datasets/kc_house_data
+csv')
+
+# função que converte de object (string) -> date
+data['date'] = pd.to_datetime(data['date'])
+
+# mostrar na tela os tipos de variáveis de cada coluna
+print(data.dtypes)
+
+# ===================================
+# Como converter os tipos de variáveis
+# ===================================
+#
+# Inteiro -> Float
+data['bedrooms'] = data['bedrooms'].astype(float)
+
+# Inteiro -> Float
+data['bedrooms'] = data['bedrooms'].astype(float)
+
+# Float -> Inteiro
+data['bedrooms'] = data['bedrooms'].astype(Int64)
+
+# Inteiro -> String
+data['bedrooms'] = data['bedrooms'].astype(str)
+
+#print(data.dtypes)
+
+# Inteiro -> Float
+data['bedrooms'] = data['bedrooms'].astype(float)
+
+# String -> Inteiro
+data['bedrooms'] = data['bedrooms'].astype(Int64)
+
+# String -> Data
+data['date'] = pd.to_datetime(data['date'])
+
+# ===================================
+# Criando novas variáveis
+# ===================================
+
+data['nome_do_meigarom'] = "meigarom"
